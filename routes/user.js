986e094d374a70a109cb9a6ee21cb5745091386f
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-06-28 15:35:58 +0530

    login signup done

--- routes/user.js
+++ routes/user.js
@@ -1,23 +1,74 @@
-import {Router} from 'express'
-import { userModel } from '../model/user.model.js'
+import { Router } from "express";
+import { userModel } from "../model/user.model.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
-const userRouter = Router()
+const userRouter = Router();
 
-userRouter.post('/signup', async(req, res) => {
-    const {name, email, password} = req.body
+userRouter.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
 
-    try{
-         const user = await userModel.create({
-        name, email, password
-    })
-    } catch(e){
-        console.log(e)
+  const existingUser = await userModel.findOne({ email });
+
+  if (existingUser) {
+    return res.status(409).json({
+      msg: "User already exists",
+    });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 5);
+
+    const user = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    console.log(user);
+  } catch (e) {
+    console.log(e);
+  }
+
+  res.json({
+    msg: "User signup successfully",
+  });
+});
+
+userRouter.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        msg: "User not found",
+      });
     }
-  
-    res.json({
-        msg: "User signup successfully"
-    })
 
-})
+    const matched = await bcrypt.compare(password, user.password);
 
-export default userRouter
+    if (!matched) {
+      return res.status(401).json({
+        msg: "Incorrect Password",
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET
+    );
+
+    return res.json({
+      msg: "Login successful",
+      token,
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      msg: "Internal Server Error",
+    });
+  }
+});
+
+export default userRouter;
